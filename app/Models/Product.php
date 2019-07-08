629a32ba06ba31c2de2a650da17e4b0ec24934ce
Your Name <you@example.com>
2019-07-08 08:56:20 +0000

    初始化秒杀模型

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -9,10 +9,12 @@ class Product extends Model
 {
     const TYPE_NORMAL = 'normal';
     const TYPE_CROWDFUNDING = 'crowdfunding';
+    const TYPE_SECKILL = 'seckill';
 
     public static $typeMap = [
         self::TYPE_NORMAL  => '普通商品',
         self::TYPE_CROWDFUNDING => '众筹商品',
+        self::TYPE_SECKILL => '秒杀商品',
     ];
     protected $fillable = ['title', 'description', 'image', 'on_sale', 'rating', 'sold_count', 'review_count', 'price', 'type', 'long_title'];
     protected $casts = [
@@ -48,6 +50,11 @@ class Product extends Model
         return $this->hasMany(ProductProperty::class);
     }
 
+    public function seckill()
+    {
+        return $this->hasOne(SeckillProduct::class);
+    }
+    
     public function getGroupedPropertiesAttribute()
     {
         return $this->properties
